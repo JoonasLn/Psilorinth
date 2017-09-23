02d1a7f8231ff262a21bf743ef5e36facded46cd
Psilorinth.Jallu <jalmari.kunnari@gmail.com>
2017-09-23 14:20:06 +0300

    Psilorinth HUD included

--- MushedRoom/Assets/Scripts/MoshroomTrigger1.cs
+++ MushedRoom/Assets/Scripts/MoshroomTrigger1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoshroomTrigger1 : MonoBehaviour {
-
+public class MoshroomTrigger1 : MonoBehaviour
+{
+    public int scoreValue = 1;
 
     void OnTriggerEnter2D(Collider2D co)
     {
+        ShroomScore.score += scoreValue;
         if (co.name == "Character")
             Destroy(gameObject);
     }
