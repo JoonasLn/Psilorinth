459d54253604c413a3f69d255a2f053642d36569
Psilorinth.Jallu <jalmari.kunnari@gmail.com>
2017-09-23 22:19:20 +0300

    Psilorinth Control Fuck Shroom

---
+++ MushedRoom/Assets/Scripts/MushroomTriggerControlFuck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoshroomTriggerControlFuck : MonoBehaviour
+{
+    public int scoreValue = 1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Is triggered");
+        ShroomScore.score += scoreValue;
+
+        if (other.name == "PlayerCharacter")
+        {
+            //other.GetComponent<NewCharacterMove>().StartSlowTimeTimer();
+            other.GetComponent<NewCharacterMove>().ControlFuckTime();
+            Destroy(gameObject);
+        }
+    }
+}
--- MushedRoom/Assets/Scripts/NewCharacterMove.cs
+++ MushedRoom/Assets/Scripts/NewCharacterMove.cs
@@ -10,22 +10,27 @@ public class NewCharacterMove : MonoBehaviour
     public float slowTime;
     public bool countSlow = false;
 
+    public float controlFuckTime;
+    public bool countFuck = false;
+
     public float horAxis;
     public float verAxis;
+    public int reverseCon = 1;
+
     // Normal Movements Variables
 
-        void Start()
+    void Start()
     {
         myRb = GetComponent<Rigidbody2D>();
     }
 
-        void FixedUpdate()
+    void FixedUpdate()
     {
         Vector3 moveDir = Vector3.zero;
         moveDir.x = Input.GetAxis("Horizontal"); // get result of AD keys in X
         moveDir.y = Input.GetAxis("Vertical"); // get result of WS keys in Z
                                                // move this object at frame rate independent speed:
-        transform.position += moveDir * runSpeed * Time.deltaTime;
+        transform.position += moveDir * reverseCon * runSpeed * Time.deltaTime;
     }
 
     // Move senteces
@@ -43,14 +48,34 @@ public class NewCharacterMove : MonoBehaviour
                 countSlow = false;
             }
         }
+
+        if (countFuck == true)
+        {
+             controlFuckTime -= Time.deltaTime;
+             if (controlFuckTime <= 0)
+             {
+                reverseCon = 1;
+                countFuck = false;
+             }
+        }
+
     }
     public void SlowTime()
     {
-        
+
         Debug.Log("Slow time");
         countSlow = true;
-        runSpeed = 5f;   
+        runSpeed = 5f;
     }
 
+    public void ControlFuckTime()
+    {
+        countFuck = true;
+        reverseCon = -2; 
+    }
+
+        
+
 }
-    
+
+
