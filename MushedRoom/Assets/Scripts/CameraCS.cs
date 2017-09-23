4e5ca5b7a6f1383ffc5241e5285e102dc6abd2f1
Eero Kinnunen <ee.jo.ki@gmail.com>
2017-09-23 20:59:30 +0300

    Kenttää ja muuta

---
+++ MushedRoom/Assets/Scripts/CameraCS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCS : MonoBehaviour {
+
+    public Transform targetObject;
+    public float speed = 10;
+    Vector3 target;
+    public float cameraDist = 10;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        target = new Vector3(targetObject.position.x, targetObject.position.y + cameraDist, targetObject.position.z - cameraDist);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
---
+++ MushedRoom/Assets/Scripts/PlayerLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour {
+    public Transform targetObject;
+    public float speed = 10;
+    Vector3 target;
+    public float aboveHeight = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        target = new Vector3(targetObject.position.x, targetObject.position.y + aboveHeight , targetObject.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
