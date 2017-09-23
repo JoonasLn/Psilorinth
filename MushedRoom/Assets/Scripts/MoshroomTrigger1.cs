45b0bcea2ef67d71a6847f787ce0c046dd5e481f
Joonas <joonas.luhtaniemi@gmail.com>
2017-09-23 13:44:28 +0300

    MoshroomTrigger

---
+++ MushedRoom/Assets/Scripts/MoshroomTrigger1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoshroomTrigger1 : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "Character")
+            Destroy(gameObject);
+    }
+}
