0e5a6a355af2df0209de7dfeca317fa3b42ed34a
Psilorinth.Jallu <jalmari.kunnari@gmail.com>
2017-09-24 15:12:31 +0300

    ExitGameTrigger Salvage

---
+++ MushedRoom/Assets/Scripts/ExitGateTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGateTrigger : MonoBehaviour
+{
+    public ShroomScore shroomScore;
+
+
+    void OnTriggerEnter2D(Collider co)
+    {
+        if (GameObject.Find("ShroomScoreText").GetComponent<ShroomScore>().publicScore >= 5)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
